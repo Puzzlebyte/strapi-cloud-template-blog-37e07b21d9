f2f7f38cf8b517c56c84bdd4c9adf829fb89727b
Chris Porter <46310006+bsxp@users.noreply.github.com>
2024-06-02 16:19:22 -0500

    undo var name changes

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('AUTH_SECRET'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN'),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
